1ef37940a7b9f5aa5e3c5109616e799c4afd66f5
Troy200 <kuriyanthomas27@gmail.com>
2026-02-11 17:57:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onresize = getw
+window.onload = getw
+
+function getw(){
+    width=window.innerWidth
+height=window.innerHeight
+
+    document.getElementById("w").innerHTML= width + "px"
+    document.getElementById("h").innerHTML=height + "px"
+
+}
